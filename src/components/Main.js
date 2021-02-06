1507968ecbf3a5bd8fc80ff4f620e6eab486fca1
singh3069 <73482745+singh3069@users.noreply.github.com>
2021-02-06 21:50:01 +0530

    Made Changes in modal.css and style.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,67 +1,65 @@
-import React, {useState , useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import Modal from "./Modal";
-import './style.css'
+import "./style.css";
 
 function Main() {
-    const [data,setState] = useState([]);
-    const [modalState, setModalState] = useState(false)
-    const [sendingData, setSendingData] = useState({})
+  const [data, setState] = useState([]);
+  const [modalState, setModalState] = useState(false);
+  const [sendingData, setSendingData] = useState({});
 
-    const openDogModalHandler = (selectedItem) =>{
-        setSendingData(selectedItem)
-        setModalState(true)
-    }
+  const openDogModalHandler = (selectedItem) => {
+    setSendingData(selectedItem);
+    setModalState(true);
+  };
 
-    const closeDogModalHandler = () =>{
-    setModalState(false)
-    }
+  const closeDogModalHandler = () => {
+    setModalState(false);
+  };
 
-    useEffect(() =>{
-        fetch('https://api.thedogapi.com/v1/breeds', {
-        headers:{
-            'x-api-key':"9232752c-89bc-4363-9439-70c7785c2b9c"
-        }
-        })
-        .then((response) => response.json())
-        .then((data) => setState(data.splice(0,20)));
-        // .then((data) => console.log(data.splice(0,20)));
+  useEffect(() => {
+    fetch("https://api.thedogapi.com/v1/breeds", {
+      headers: {
+        "x-api-key": "9232752c-89bc-4363-9439-70c7785c2b9c",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setState(data.splice(0, 20)));
+    // .then((data) => console.log(data.splice(0,20)));
+  }, []);
 
-    },[]);
+  return (
+    <div>
+      <div className="nav-bar">
+        <u>
+          <h1>Know About Your Fav. Pet(🐕)</h1>
+        </u>
+      </div>
 
-    return (
-        
-        <div>
-            <div className="nav-bar"><u><h1>Know About Your Fav. Pet(🐕)</h1></u></div>
+      <div className="nameImg-div">
+        {data.map((item) => {
+          return (
+            <div>
 
-            <div className="nameImg-div">
-            
-                {
-                data.map((item)=>{
+              <div className="content-div" key={item.id} onClick={() => openDogModalHandler(item)} >
+                  <div className="pic-name-div">
+                      <img className="images" alt={item.name} src={item.image.url} />
+                        <h4 className="name">{item.name}</h4>
+                  </div>
+                
+              </div>
 
-                    return (
-                        <div>
-                    <div className="content-div" key={item.id} onClick={()=>openDogModalHandler(item)} > 
-                    
-                    <img className="images" alt={item.name} src={item.image.url}/>
-                    <div className="name">
-                        <h4>{item.name}</h4>
-                    </div>
-                    </div>
-                            {modalState &&
-                            <Modal dogData={sendingData} closeHandler={closeDogModalHandler}/>}
-
-                    </div>
-                    
-                    )
-                })
-                }
-
-
-            
-        </div>
-        </div>
-        
-    )
+              {modalState && (
+                <Modal
+                  dogData={sendingData}
+                  closeHandler={closeDogModalHandler}
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,45 +1,54 @@
-import './modal.css'
-const Modal = (props) =>{
-  const {dogData, closeHandler} = props
-  return(
+import "./modal.css";
+const Modal = (props) => {
+  const { dogData, closeHandler } = props;
+  return (
     <div>
-
       {/* <div>
         <h1>{dogData.name}</h1>
       </div> */}
 
-    <div className="modalContainer">
-      <span onClick={closeHandler} className="closeBtn"></span>
-      
+      <div className="modalContainer">
+        <span onClick={closeHandler} className="closeBtn"></span>
 
-      <div className="img-div">
-      <img className="modalDogImage" alt="dogimages" src={dogData.image.url}/>
+        <div className="img-div">
+          <img
+            className="modalDogImage"
+            alt="dogimages"
+            src={dogData.image.url}
+          />
+        </div>
+
+        <div className="name-nd-abut-div">
+          <div className="name-div">
+            <h1>{dogData.name}</h1>
+          </div>
+
+          <div className="abut-div">
+            <p>
+              <u>Temperament</u>:-&nbsp;
+              {dogData.temperament}.
+            </p>
+            <p>
+              <u>Origin</u>:-&nbsp;
+              {dogData.origin ? dogData.origin : <i>Not Found</i>}.
+            </p>
+            <p>
+              <u>Bred-For</u>:-&nbsp;
+              {dogData.bred_for ? dogData.bred_for : <i>Not Found</i>}.
+            </p>
+            <p>
+            <u>Breed-Group</u>:-&nbsp;
+              {dogData.breed_group ? dogData.breed_group : <i>Not Found</i>}.
+            </p>
+            <p>
+              <u>Life-Span</u>:-&nbsp;
+              {dogData.life_span ? dogData.life_span : <i>Not Found</i>}.
+            </p>
+          </div>
+        </div>
       </div>
-      
-      <div className="name-nd-abut-div">
-      <div className="name-div">
-        <h1>{dogData.name}</h1>
-      </div> 
-
-      <div className="abut-div">
-
-      <p><b>Temperament:-</b>{dogData.temperament}.</p>
-      <p><b>Origin:-</b>{dogData.origin ? dogData.origin : <i>Not Found😕</i>}.</p>
-      <p><b>Bred-For:-</b>{dogData.bred_for ? dogData.bred_for : <i>Not Found😕</i>}.</p>
-      <p><b>Breed-Group:-</b>{dogData.breed_group ? dogData.breed_group : <i>Not Found😕</i>}.</p>
-      <p><b>Life-Span:-</b>{dogData.life_span ? dogData.life_span : <i>Not Found😕</i>}.</p>
-
-      </div>
-
-      </div>
-      
-
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-
-export default Modal
-
+export default Modal;
